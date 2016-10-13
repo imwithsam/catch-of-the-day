a858faf0b4e49d562ab32aa256014392c8f5d4f9
Samson Brock <SamsonLB@gmail.com>
2016-10-12 21:57:50 -0700

    Add Firebase to persist fishes state.

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAv5rm8c_42ytBgekSAxlHMJxs805rlX5w",
+  authDomain: "catch-of-the-day-a6568.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-a6568.firebaseio.com"
+})
+
+export default base
